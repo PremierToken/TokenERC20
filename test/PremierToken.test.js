ffed804f553c09bda07eff9823d290ae311cb76c
Yasmim Vieira <yasmim@4cadia.com>
2019-12-19 18:02:08 -0300

    Finish test at burn

--- test/PremierToken.test.js
+++ test/PremierToken.test.js
@@ -7,7 +7,7 @@ chai.use(solidity);
 
 const {expect} = chai;
 
-async function createPremierToken(
+async function createContract(
   wallet,
   {name = 'Premier Token', symbol = 'PTK', decimals = 18} = {}
 ) {
@@ -20,36 +20,61 @@ describe('Premier Token', () => {
 
   describe('Mint', () => {
     it('should allow owner to mint to client address', async () => {
-      const premierToken = await createPremierToken(owner);
+      const PremierToken = await createContract(owner);
 
-      await premierToken.mintTo(client.address, 1000);
+      await PremierToken.mintTo(client.address, 1000);
 
-      const clientBalance = await premierToken.balanceOf(client.address);
-      const totalSupply = await premierToken.totalSupply();
+      const clientBalance = await PremierToken.balanceOf(client.address);
+      const totalSupply = await PremierToken.totalSupply();
 
       expect(clientBalance).to.equal(1000);
       expect(totalSupply).to.equal(1000);
     });
 
     it('should allow minter to mint to client address', async () => {
-      const contractAsOwner = await createPremierToken(owner);
+      const contractAsOwner = await createContract(owner);
       await contractAsOwner.addMinter(minter.address);
 
       const contractAsMinter = contractAsOwner.connect(minter);
 
       await contractAsMinter.mintTo(client.address, 1000);
 
-      const clientBalnce = await contractAsMinter.balanceOf(client.address);
+      const clientBalance = await contractAsMinter.balanceOf(client.address);
       const totalSupply = await contractAsMinter.totalSupply();
 
-      expect(clientBalnce).to.equal(1000);
+      expect(clientBalance).to.equal(1000);
       expect(totalSupply).to.equal(1000);
     });
   });
 
   describe('Burn', () => {
-    it.todo('should allow owner to mint to client address');
+    it('should allow owner to burn to client address', async () => {
+      const PremierToken = await createContract(owner);
+      await PremierToken.mintTo(client.address, 1000);
+      await PremierToken.burnFrom(client.address, 800);
 
-    it.todo('should allow minter to mint to client address');
+      const clientBalance = await PremierToken.balanceOf(client.address);
+      const totalSupply = await PremierToken.totalSupply();
+
+      expect(clientBalance).to.equal(200);
+      expect(totalSupply).to.equal(200);
+    });
+
+    it('should allow minter to burn to client address', async () => {
+      const contractAsOwner = await createContract(owner);
+      await contractAsOwner.addBurner(burner.address);
+
+      const contractAsBurner = contractAsOwner.connect(owner);
+      await contractAsBurner.mintTo(client.address, 1000);
+
+      contractAsOwner.connect(burner);
+      await contractAsBurner.burnFrom(client.address, 800);
+
+      const clientBalance = await contractAsBurner.balanceOf(client.address);
+      const totalSupply = await contractAsBurner.totalSupply();
+
+      expect(clientBalance).to.equal(200);
+      expect(totalSupply).to.equal(200);
+    });
   });
 });
